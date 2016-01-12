276f865761b0edd84bbd98284d3cdeb1eea67ac1
Michael Eder <kontakt@michael-eder.net>
2016-01-12 13:46:51 +0100

    More colors

--- pyjeopardy/config.py
+++ pyjeopardy/config.py
@@ -2,8 +2,19 @@ from PySide.QtGui import QColor
 
 COLORS=[
     ('red', QColor(255, 0, 0)),
-    ('green', QColor(0, 255, 0)),
+    ('lime', QColor(0, 255, 0)),
     ('blue', QColor(0, 0, 255)),
+    ('yellow', QColor(255,255,0)),
+    ('cyan', QColor(0,255,255)),
+    ('magenta', QColor(255,0,255)),
+    ('silver', QColor(192,192,192)),
+    ('gray', QColor(128,128,128)),
+    ('maroon', QColor(128,0,0)),
+    ('olive', QColor(128,128,0)),
+    ('green', QColor(0,128,0)),
+    ('purple', QColor(128,0,128)),
+    ('teal', QColor(0,128,128)),
+    ('navy', QColor(0,0,128))
 ]
 
 def get_color_for_name(name):
